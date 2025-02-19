9f07343c9f69f019bc65ebc4e63ced14c35ff304
Andrei Orlov <aaorlov1@gmail.com>
2025-02-19 15:52:52 +0300

    Update qf-test-runner

--- qf-test-runner/src/main.rs
+++ qf-test-runner/src/main.rs
@@ -2,6 +2,9 @@ use clap::Parser;
 use tracing_subscriber::prelude::*;
 
 use polkavm::{Config as PolkaVMConfig, Engine, Linker, Module as PolkaVMModule, ProgramBlob, State};
+use polkavm::Caller;
+use polkavm::InterruptKind;
+use polkavm::Reg;
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -33,7 +36,10 @@ fn main() {
     let engine = Engine::new(&config).unwrap();
     let module = PolkaVMModule::from_blob(&engine, &Default::default(), blob).unwrap();
 
-    let linker: Linker = Linker::new();
+    let mut linker: Linker = Linker::new();
+
+    linker.define_typed("foo", |caller: Caller| -> u32 { caller.user_data.foo }).unwrap();
+    linker.define_typed("transfer", |caller: Caller| -> u32 { caller.user_data.address[2].into() }).unwrap();
 
     // Link the host functions with the module.
     let instance_pre = linker.instantiate_pre(&module).unwrap();
@@ -41,11 +47,53 @@ fn main() {
     // Instantiate the module.
     let mut instance = instance_pre.instantiate().unwrap();
 
-    let mut state = State::new(vec![]);
+    let mut state = State::new(
+         vec![1,2,3],
+         100,
+    );
+
+    // linker.define_typed("foo", |caller: Caller| -> u32 { 42 }).unwrap();
+    // linker.define_typed("foo", |caller: Caller| -> u32 { 42 }).unwrap();
+    // linker.define_typed("transfer", |caller: Caller| -> u32 { caller.user_data.address[2].into() }).unwrap();
 
     let res = instance
-        .call_typed_and_get_result::<u32, (u32, u32)>(&mut state, "add_numbers", (1, 2))
-        .unwrap();
+        .call_typed_and_get_result::<u32, (u32, u32)>(&mut state, "add_numbers", (1, 2));
+    
+    tracing::info!("res: {:?}", res);
 
-    tracing::info!("Result: {:?}", res);
+    tracing::info!("Result: {:?}", res.unwrap());
+    
+    let entry_point = module.exports().find(|export| export == "add_numbers").unwrap().program_counter();
+    let mut instance = module.instantiate().unwrap();
+    instance.set_next_program_counter(entry_point);
+    instance.set_reg(Reg::A0, 1);
+    instance.set_reg(Reg::A1, 10);
+    instance.set_reg(Reg::RA, polkavm::RETURN_TO_HOST);
+    instance.set_reg(Reg::SP, module.default_sp());
+
+    println!("Calling into the guest program (low level):");
+    loop {
+        let interrupt_kind = instance.run().unwrap();
+        match interrupt_kind {
+            InterruptKind::Finished => break,
+            InterruptKind::Ecalli(num) => {
+                let Some(name) = module.imports().get(num) else {
+                    panic!("unexpected external call: {num}");
+                };
+
+                println!("name: {}", name);
+
+                if name == "foo" {
+                    instance.set_reg(Reg::A0, 1000);
+                } else if name == "transfer" {
+                    instance.set_reg(Reg::A0, 3);
+                } else {
+                    panic!("unexpected external call: {name} ({num})")
+                }
+            }
+            _ => panic!("unexpected interruption: {interrupt_kind:?}"),
+        }
+    }
+
+    println!("  1 + 10 + 1000 + 3 = {}", instance.reg(Reg::A0));
 }
