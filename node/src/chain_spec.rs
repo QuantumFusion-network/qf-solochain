c37fd5c5d949c0d0e3cc1a1c62cca6e69bf40bed
Sviatoslav Alekseev <sviatoslav.alekseev@proton.me>
2025-04-04 14:24:04 +0500

    Apply controller changes

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -119,8 +119,8 @@ fn testnet_genesis(
                 .iter()
                 .map(|x| {
                     (
-                        x.0.clone(),
-                        x.0.clone(),
+                        x.1.clone(),
+                        x.1.clone(),
                         qf_runtime::SessionKeys {
                             spin: x.2.clone(),
                             grandpa: x.3.clone(),
@@ -134,9 +134,9 @@ fn testnet_genesis(
             "validatorCount": initial_authorities.len(),
             "stakers": initial_authorities
                 .iter()
-                .map(|x| (x.0.clone(), x.0.clone(), STASH, pallet_staking::StakerStatus::<AccountId>::Validator))
+                .map(|x| (x.1.clone(), x.1.clone(), STASH, pallet_staking::StakerStatus::<AccountId>::Validator))
                 .collect::<Vec<_>>(),
-            "invulnerables": initial_authorities.iter().map(|x| x.0.clone()).collect::<Vec<_>>(),
+            "invulnerables": initial_authorities.iter().map(|x| x.1.clone()).collect::<Vec<_>>(),
             "forceEra": pallet_staking::Forcing::NotForcing,
             "slashRewardFraction": sp_runtime::Perbill::from_percent(10),
         },
