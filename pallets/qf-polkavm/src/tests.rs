f96f6b58fa3618d7cd7ef758917b734625eaacb9
Andrei Orlov <aaorlov1@gmail.com>
2025-05-23 16:44:29 +0300

    cargo fmt

--- pallets/qf-polkavm/src/tests.rs
+++ pallets/qf-polkavm/src/tests.rs
@@ -1,4 +1,4 @@
-use crate::{mock::*, Error, Event, CodeAddress};
+use crate::{CodeAddress, Error, Event, mock::*};
 use frame_support::{assert_noop, assert_ok};
 
 const ALICE: AccountId = 1;
@@ -9,8 +9,14 @@ const VERSION: u64 = 1;
 fn upload_invalid_blob_should_not_work() {
 	new_test_ext().execute_with(|| {
 		System::set_block_number(1);
-		assert_noop!(QfPolkaVM::upload(RuntimeOrigin::signed(ALICE), vec![]), Error::<Test>::ProgramBlobParsingFailed);
-		assert_noop!(QfPolkaVM::upload(RuntimeOrigin::signed(ALICE), vec![1,2,3]), Error::<Test>::ProgramBlobParsingFailed);
+		assert_noop!(
+			QfPolkaVM::upload(RuntimeOrigin::signed(ALICE), vec![]),
+			Error::<Test>::ProgramBlobParsingFailed
+		);
+		assert_noop!(
+			QfPolkaVM::upload(RuntimeOrigin::signed(ALICE), vec![1, 2, 3]),
+			Error::<Test>::ProgramBlobParsingFailed
+		);
 	})
 }
 
@@ -18,7 +24,10 @@ fn upload_invalid_blob_should_not_work() {
 fn upload_big_blob_should_not_work() {
 	new_test_ext().execute_with(|| {
 		System::set_block_number(1);
-		assert_noop!(QfPolkaVM::upload(RuntimeOrigin::signed(ALICE), [0; 131073].to_vec()), Error::<Test>::ProgramBlobIsTooLarge);
+		assert_noop!(
+			QfPolkaVM::upload(RuntimeOrigin::signed(ALICE), [0; 131073].to_vec()),
+			Error::<Test>::ProgramBlobIsTooLarge
+		);
 	})
 }
 
@@ -26,13 +35,19 @@ fn upload_big_blob_should_not_work() {
 fn upload_valid_blob_should_work() {
 	new_test_ext().execute_with(|| {
 		System::set_block_number(1);
-		assert_ok!(QfPolkaVM::upload(RuntimeOrigin::signed(ALICE), include_bytes!("seeds/hello-qf-polkavm.polkavm").to_vec()));
+		assert_ok!(QfPolkaVM::upload(
+			RuntimeOrigin::signed(ALICE),
+			include_bytes!("seeds/hello-qf-polkavm.polkavm").to_vec()
+		));
 		assert_eq!(CodeAddress::<Test>::get((ALICE, VERSION)), Some(CONTRACT_ADDRESS));
-		System::assert_last_event(Event::ProgramBlobUploaded {
-			who: ALICE,
-			contract_address: CONTRACT_ADDRESS,
-			version: VERSION,
-			exports: vec!["main".bytes().collect()],
-		}.into());
+		System::assert_last_event(
+			Event::ProgramBlobUploaded {
+				who: ALICE,
+				contract_address: CONTRACT_ADDRESS,
+				version: VERSION,
+				exports: vec!["main".bytes().collect()],
+			}
+			.into(),
+		);
 	})
 }
