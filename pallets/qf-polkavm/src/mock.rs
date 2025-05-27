0a42f9e4104bb8457b4164d376fad5f0409ea2a3
Andrei Orlov <aaorlov1@gmail.com>
2025-05-27 12:29:52 +0300

    Fix comment

--- pallets/qf-polkavm/src/mock.rs
+++ pallets/qf-polkavm/src/mock.rs
@@ -80,5 +80,8 @@ impl pallet::Config for Test {
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-	frame_system::GenesisConfig::<Test>::default().build_storage().unwrap().into()
+	frame_system::GenesisConfig::<Test>::default()
+		.build_storage()
+		.expect("Genesis storage can be builded; qed")
+		.into()
 }
