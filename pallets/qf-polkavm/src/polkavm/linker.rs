5c492ce21937069b8ccedceefcda89f1a7127429
Andrei Orlov <aaorlov1@gmail.com>
2025-06-04 11:37:04 +0300

    cargo fmt

--- pallets/qf-polkavm/src/polkavm/linker.rs
+++ pallets/qf-polkavm/src/polkavm/linker.rs
@@ -1,8 +1,8 @@
 extern crate alloc;
 
 use crate::{
-	BalanceOf, CodeStorageKey, StorageValue, CodeVersion, Config as PalletConfig,
-	MutatingStorageOperation, StorageKey,
+	BalanceOf, CodeStorageKey, CodeVersion, Config as PalletConfig, MutatingStorageOperation,
+	StorageKey, StorageValue,
 	polkavm::{
 		Error, InterruptKind, Module, ProgramCounter, RawInstance, Reg, api::RegValue, error::bail,
 		program::ProgramSymbol,
