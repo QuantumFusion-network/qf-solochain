839f3a07006f8cacc759151be7f026b412be0f98
Alisher A. Khassanov <a.khssnv@gmail.com>
2025-07-01 20:02:23 +0500

    Fix network backend panic

--- node/src/command.rs
+++ node/src/command.rs
@@ -180,8 +180,7 @@ pub fn run() -> sc_cli::Result<()> {
 		None => {
 			let runner = cli.create_runner(&cli.run)?;
 			runner.run_node_until_exit(|config| async move {
-				// TODO(khssnv): remove `expect` at `stable2506` or later.
-				match config.network.network_backend.expect("missing network backend") {
+				match config.network.network_backend {
 					sc_network::config::NetworkBackendType::Libp2p => service::new_full::<
 						sc_network::NetworkWorker<
 							qf_runtime::opaque::Block,
