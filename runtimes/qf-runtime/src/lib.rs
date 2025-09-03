472300274895b92c4197f622e42350c870be99d3
Alisher A. Khassanov <a.khssnv@gmail.com>
2025-09-03 18:23:59 +0500

    Bump fastchain spec_version

--- runtimes/qf-runtime/src/lib.rs
+++ runtimes/qf-runtime/src/lib.rs
@@ -65,11 +65,9 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: Cow::Borrowed("QF-runtime"),
 	authoring_version: 1,
 	// The version of the runtime specification. A full node will not attempt to use its native
-	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
-	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
-	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
-	//   the compatible custom types.
-	spec_version: 101,
+	// runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
+	// `spec_version`, and `authoring_version` are the same between Wasm and native.
+	spec_version: 102,
 	impl_version: 1,
 	apis: apis::RUNTIME_API_VERSIONS,
 	transaction_version: 1,
