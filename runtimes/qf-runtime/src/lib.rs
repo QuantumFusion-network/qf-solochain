00b5795d8ba27cd58be3c19e95475158ae567af1
Alisher A. Khassanov <a.khssnv@gmail.com>
2025-09-18 18:39:16 +0500

    Bump spec_version

--- runtimes/qf-runtime/src/lib.rs
+++ runtimes/qf-runtime/src/lib.rs
@@ -67,7 +67,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// The version of the runtime specification. A full node will not attempt to use its native
 	// runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
 	// `spec_version`, and `authoring_version` are the same between Wasm and native.
-	spec_version: 102,
+	spec_version: 103,
 	impl_version: 1,
 	apis: apis::RUNTIME_API_VERSIONS,
 	transaction_version: 1,
