e6499752d8b5d515639556a965a0dc110562c460
Alisher A. Khassanov <a.khssnv@gmail.com>
2025-12-09 16:46:52 +0500

    Specify RelayParentOffset for parachain_system
    Breaking change https://github.com/paritytech/polkadot-sdk/pull/8299/files#diff-5bc5b887e7b610d6d96f539831e41e6ca67f928a6cda5a1fad02166974ac6a75R311.

--- runtimes/parachain/src/configs/mod.rs
+++ runtimes/parachain/src/configs/mod.rs
@@ -213,7 +213,7 @@ impl cumulus_pallet_parachain_system::Config for Runtime {
 	type ReservedXcmpWeight = ReservedXcmpWeight;
 	type CheckAssociatedRelayNumber = RelayNumberMonotonicallyIncreases;
 	type ConsensusHook = ConsensusHook;
-	type SelectCore = cumulus_pallet_parachain_system::DefaultCoreSelector<Runtime>;
+	type RelayParentOffset = ConstU32<0>;
 }
 
 impl parachain_info::Config for Runtime {}
