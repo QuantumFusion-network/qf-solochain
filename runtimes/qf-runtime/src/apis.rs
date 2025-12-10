a4c1b1117d41697e29d55c2cf59c3e01a85f917b
Alisher A. Khassanov <a.khssnv@gmail.com>
2025-12-10 15:18:03 +0500

    Remove deprecated RuntimeString

--- runtimes/qf-runtime/src/apis.rs
+++ runtimes/qf-runtime/src/apis.rs
@@ -256,7 +256,7 @@ impl_runtime_apis_plus_revive!(
 
 		fn dispatch_benchmark(
 			config: frame_benchmarking::BenchmarkConfig
-		) -> Result<Vec<frame_benchmarking::BenchmarkBatch>, sp_runtime::RuntimeString> {
+		) -> Result<Vec<frame_benchmarking::BenchmarkBatch>, alloc::string::String> {
 			use frame_benchmarking::{baseline, BenchmarkBatch};
 			use sp_storage::TrackedStorageKey;
 			use frame_system_benchmarking::Pallet as SystemBench;
